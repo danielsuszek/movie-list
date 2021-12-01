034610bc96f96f7c3c2718ff9794e02e638e2cae
danielsuszek <danielsuszek1@gmail.com>
2021-12-01 13:36:38 +0100

    Update before adding genre to show

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -5,7 +5,9 @@ import classes from "./ListItem.module.scss"
 const ListItem = ({id, name, image, rating}) => {
   return (
     <Link to={`/singleshow/${id}`} className={classes.listItem}>
-      <img src={image} alt={name} />
+      <div className={classes.listItem__imgWrapper}>
+        <img src={image} alt={name} />
+      </div>
       <div >
         <h4 title={name}>{name}</h4>
         <h5>{rating}</h5>
