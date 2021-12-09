4ed5bd0e8584fbd4661572ccae614c5b93d62f37
danielsuszek <danielsuszek1@gmail.com>
2021-12-09 10:46:19 +0100

    Change folder name to scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "./sass/main-import.scss";
+import "./scss/main-import.scss";
 import ShowsState from "./context/ShowsState";
 
 ReactDOM.render(
